f43c159ae24b857ecadfbddbb8b2728cdc3a1914
Youssef Magdy <yma.youssef.21@gmail.com>
2024-05-04 00:00:44 +0300

    added validation errors styles

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,9 +1,17 @@
-const InputField = ({ label, placeholder, value, setBirthDate }) => {
+const InputField = ({
+  label,
+  placeholder,
+  value,
+  setBirthDate,
+  errorStatus,
+}) => {
+  // check if there is any invalid value / case
+  const isInvalid = Object.values(errorStatus).includes(true);
   return (
     <div>
       <label
         htmlFor={label}
-        className="mb-1 block text-[10px] font-bold uppercase tracking-[0.2em] text-smokey-grey"
+        className={`block text-[0.625rem] font-bold uppercase tracking-[0.2em] ${isInvalid ? "text-light-red" : "text-smokey-grey"}`}
       >
         {label}
       </label>
@@ -14,25 +22,53 @@ const InputField = ({ label, placeholder, value, setBirthDate }) => {
         id={label}
         value={value === 0 ? "" : value}
         required
-        // min={(label === "day" || "month") && 1}
-        // max={(label === "day" && 31) || (label === "month" && 12)}
+        min={1}
+        max={label === "day" ? 31 : label === "month" ? 12 : 9999}
         onChange={(e) => {
           setBirthDate((prevBirthDate) => {
             return { ...prevBirthDate, [e.target.name]: e.target.value };
           });
-          // console.log(e.target.name);
         }}
-        className="w-28 rounded-md border border-light-grey px-3
-         py-2 text-xl font-bold text-off-black outline-none
+        className={`my-1 w-28 rounded-md
+        border px-3 py-2 text-xl
+        font-bold
+        text-off-black
+        outline-none
         transition
         duration-200
-        ease-in-out
-        placeholder:text-lg
+        ease-in-out placeholder:text-lg
         placeholder:font-bold
-        placeholder:text-smokey-grey placeholder:opacity-80
+        placeholder:text-smokey-grey
+        placeholder:opacity-80
         focus:border-purble
-        "
-      ></input>
+        ${isInvalid ? "border-light-red" : "border-light-grey"}
+        `}
+      />
+      {(errorStatus.emptyFields && (
+        <p className="text-[0.625rem] italic text-light-red">
+          This field is required
+        </p>
+      )) ||
+        (errorStatus.notValidMonth && label === "month" && (
+          <p className="text-[0.625rem] italic text-light-red">
+            Must be a valid {label}
+          </p>
+        )) ||
+        (errorStatus.notValidDay && label === "day" && (
+          <p className="text-[0.625rem] italic text-light-red">
+            Must be a valid {label}
+          </p>
+        )) ||
+        (errorStatus.notPastYear && label === "year" && (
+          <p className="text-[0.625rem] italic text-light-red">
+            Must be in the past
+          </p>
+        )) ||
+        (errorStatus.notValidDate && label === "day" && (
+          <p className="text-[0.625rem] italic text-light-red">
+            Must be a valid date
+          </p>
+        ))}
     </div>
   );
 };
--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -6,18 +6,31 @@ const InputSection = ({ setResultDate }) => {
     month: 0,
     year: 0,
   });
-
+  const [errorStatus, setErrorStatus] = useState({
+    emptyFields: false,
+    notValidDate: false,
+    notPastYear: false,
+    notValidDay: false,
+    notValidMonth: false,
+  });
   const { day, month, year } = birthDate;
   const currentDate = new Date();
 
   const isValidDate = () => {
-    return month >= 1 && month <= 12 && day >= 1 && day <= validateMonthDays();
+    // return month >= 1 && month <= 12 && day >= 1 && day <= getMonthDays();
+    return isValidMonth() && isValidDay();
   };
   const isLeapYear = () => {
     const y = parseInt(year);
     return (y % 4 === 0 && y % 100 !== 0) || y % 400 === 0;
   };
-  const validateMonthDays = () => {
+  const isValidMonth = () => {
+    return month >= 1 && month <= 12;
+  };
+  const isValidDay = () => {
+    return day >= 1 && day <= getMonthDays();
+  };
+  const getMonthDays = () => {
     const m = parseInt(month);
     switch (m) {
       case 2:
@@ -35,6 +48,9 @@ const InputSection = ({ setResultDate }) => {
     // date string short format used > MM/DD/YYYY
     const inputDate = new Date(`${month}/${day}/${year}`);
     return inputDate < currentDate;
+  };
+  const isPastYear = () => {
+    return year < currentDate.getFullYear();
   };
   const calculateAge = () => {
     const currentYear = currentDate.getFullYear();
@@ -56,7 +72,74 @@ const InputSection = ({ setResultDate }) => {
       months: resultMonths,
       years: resultYears,
     });
+    setErrorStatus({
+      emptyFields: false,
+      notValidDate: false,
+      notPastYear: false,
+      notValidDay: false,
+      notValidMonth: false,
+    });
   };
+  const isEmptyFields = () => {
+    return day == "" || month == "" || year == "";
+  };
+  // const handleError = () => {
+  //   if (isEmptyFields()) {
+  //     setResultDate({
+  //       days: 0,
+  //       months: 0,
+  //       years: 0,
+  //     });
+  //     setErrorStatus((prev) => {
+  //       return { ...prev, emptyFields: true };
+  //     });
+  //   } else if (!isValidDate()) {
+  //     const m = isValidMonth();
+  //     const d = isValidDay();
+  //     setResultDate({
+  //       days: 0,
+  //       months: 0,
+  //       years: 0,
+  //     });
+  //     setErrorStatus({
+  //       emptyFields: false,
+  //       notPastYear: false,
+  //       notValidDay: !d,
+  //       notValidMonth: !m,
+  //     });
+  //   } else if (!isPastDate()) {
+  //     const p = isPastYear();
+  //     console.log(p);
+  //     setErrorStatus((prev) => {
+  //       return { ...prev, notPastYear: !p };
+  //     });
+  //   }
+  // };
+  const handleError = () => {
+    const m = isValidMonth();
+    const p = isPastYear();
+    const t = isValidDate();
+    const isValidInputDay = day >= 1 && day <= 31;
+    setResultDate({
+      days: 0,
+      months: 0,
+      years: 0,
+    });
+    if (isEmptyFields()) {
+      setErrorStatus((prev) => {
+        return { ...prev, emptyFields: true };
+      });
+    } else {
+      setErrorStatus({
+        emptyFields: false,
+        notValidDate: !t,
+        notPastYear: !p,
+        notValidDay: !isValidInputDay,
+        notValidMonth: !m,
+      });
+    }
+  };
+
   return (
     <div className="relative flex w-[500px] gap-4 border-b border-light-grey pb-7">
       <InputField
@@ -64,38 +147,36 @@ const InputSection = ({ setResultDate }) => {
         placeholder={"DD"}
         value={day}
         setBirthDate={setBirthDate}
+        errorStatus={errorStatus}
       />
       <InputField
         label={"month"}
         placeholder={"MM"}
         value={month}
         setBirthDate={setBirthDate}
+        errorStatus={errorStatus}
       />
       <InputField
         label={"year"}
         placeholder={"YYYY"}
         value={year}
         setBirthDate={setBirthDate}
+        errorStatus={errorStatus}
       />
       <button
         type="button"
-        className="hover: absolute -bottom-8 right-0
+        className="absolute -bottom-8 right-0
         flex
         h-16 w-16
-      items-center  justify-center rounded-full bg-purble
-      transition
+        items-center  justify-center rounded-full bg-purble
+        transition
         duration-300
-       
         ease-in-out hover:bg-off-black"
         onClick={() => {
           console.log(day, month, year);
-          isValidDate() && isPastDate()
+          isValidDate() && isPastDate() && !isEmptyFields()
             ? calculateAge()
-            : setResultDate({
-                days: 0,
-                months: 0,
-                years: 0,
-              });
+            : handleError();
           // console.log(isPastDate());
           // console.log(validateMonth());
           // console.log(typeof month);
